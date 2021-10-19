967f233a68cfbf658c823aab67bb97f8c4314afa
ISOLA2069 <isola.tong@gmail.com>
2021-10-19 15:41:49 -0700

    Update

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+
+// Display an image of soil
+// Accept user left click
+// Load random image of random flora and fauna from the Philippine forest
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ *  Author: Isola Tong <isotong@ucsc.edu>
+ *  Created: 19 October
+ *  License: Public Domain
+ */
+
+ // Define Variables
+ make = "Toyota";
+ model = "Camry";
+ color = "Black";
+ year = 2008;
+
+ // Calculate
+ age = 2021 - year;
+
+ // Output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "years<br>");
